e82d7665a2b884c437dff82a9cc90950ce11fe58
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-06-10 17:04:44 -0300

    Create database.py file

---
+++ core/database.py
@@ -0,0 +1,16 @@
+from core.configs import settings
+from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
+                                    create_async_engine)
+from sqlalchemy.orm import sessionmaker
+
+engine: AsyncEngine = create_async_engine(settings.DB_URL)
+
+
+Session: AsyncSession = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_= AsyncSession,
+    bind=engine
+
+)
